e124bbd4fa2c1b3cd84e030f6d7f51d7495b89da
HaydenLiao <liaohaitun@gmail.com>
2020-06-29 12:42:21 -0400

    Initial commit

---
+++ src/algorithm/easy/easy.java
@@ -0,0 +1,31 @@
+package algorithm.easy;
+
+public class easy {
+    public static void main(String[] args){
+
+        System.out.println("hello world");
+        easy t=new easy();
+        t.test();
+        int[] tw=new int[]{2,3};
+        t.twoSum(new int[]{2, 3}, 3);
+
+    }
+    public int test(){
+        System.out.println("hi");
+        return 1;
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        /**
+         * leetcode 1
+         * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+         *
+         * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+         *
+
+         */
+        int[] ret=new int[2];
+        return ret;
+
+    }
+}
