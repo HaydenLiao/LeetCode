2914f7d695b5b1fc71e63223c23f91543263eed0
HaydenLiao <liaohaitun@gmail.com>
2020-08-16 20:33:34 -0400

    update
    question 26

--- src/algorithm/easy/easy.java
+++ src/algorithm/easy/easy.java
@@ -7,10 +7,41 @@ public class easy {
     public static void main(String[] args){
 
         easy t=new easy();
-        String[] s= new String[]{};
-        System.out.println(t.longestCommonPrefix(s));
+        int[] s= new int[]{};
+        System.out.println(t.removeDuplicates(s));
 
     }
+
+
+
+//Question 26---------------------------------------------
+    public int removeDuplicates(int[] nums) {
+        /**
+         * 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+         * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+         * 时间复杂度O(n)
+         * 空间复杂度O(1)
+         */
+        if(nums.length<2) return nums.length;
+        int tag1=0;
+        int tag2=1;
+        int insert=0;
+        while(tag2<nums.length){
+            if(nums[tag1]==nums[tag2]){
+                tag1=tag2;
+                tag2++;
+            }
+            else{
+                insert++;
+                nums[insert]=nums[tag2];
+                tag1=tag2;
+                tag2++;
+
+            }
+        }
+        return insert+1;
+    }
+
 
 //Question 14---------------------------------------------
     public String longestCommonPrefix(String[] strs) {
