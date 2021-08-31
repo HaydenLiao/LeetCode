2f24d48837e51823c5938293b778d72ca5be9d04
HaydenLiao <liaohaitun@gmail.com>
2021-08-31 14:14:37 -0400

    leetcode-Q1109.Corporate Flight Bookings
    brute force

--- src/algorithm/easy/easy.java
+++ src/algorithm/easy/easy.java
@@ -15,18 +15,54 @@ public class easy {
     public static void main(String[] args){
 
         easy t=new easy();
-        int[] s= new int[]{3,1,2,10,1};
-        int target=2;
-        String haystack = "      ";
-        String needle = "abaaaa";
-        int[] sum=t.runningSum(s);
-        for(int i:sum){
+        int[][] s= new int[][]{{1,2,10},{2,3,20},{2,5,25}};
+        int n=5;
+
+        int[] ans=t.corpFlightBookings(s,n);
+        for(int i:ans){
             System.out.println(i);
         }
 
 //        System.out.println(t.longestPalindrome(needle));
 
     }
+
+
+    //Question 1109.Corporate Flight Bookings
+    /**
+     * There are n flights that are labeled from 1 to n.
+     *
+     * You are given an array of flight bookings bookings, where bookings[i] = [firsti, lasti, seatsi] represents a booking for flights firsti through lasti (inclusive) with seatsi seats reserved for each flight in the range.
+     *
+     * Return an array answer of length n, where answer[i] is the total number of seats reserved for flight i.
+     * eg:
+     * Input: bookings = [[1,2,10],[2,3,20],[2,5,25]], n = 5
+     * Output: [10,55,45,25,25]
+     * Explanation:
+     * Flight labels:        1   2   3   4   5
+     * Booking 1 reserved:  10  10
+     * Booking 2 reserved:      20  20
+     * Booking 3 reserved:      25  25  25  25
+     * Total seats:         10  55  45  25  25
+     * Hence, answer = [10,55,45,25,25]
+     *
+     */
+//    int[][] s= new int[][]{{1,2,10},{2,3,20},{2,5,25}};
+//    int n=5;
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        /**
+         * 第一遍解法：暴力遍历
+         */
+        int[] ans=new int[n];
+        for(int i=0;i<bookings.length;i++){
+            for(int j=bookings[i][0];j<=bookings[i][1];j++){
+                ans[j-1]=ans[j-1]+bookings[i][2];
+            }
+        }
+        return ans;
+    }
+
+
 
     //Question 1480. Running Sum of 1d Array
     /**
