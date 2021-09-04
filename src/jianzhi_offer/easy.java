c9fe39fd3f54e8f8b365a65c8d82d9edc62f9bb1
HaydenLiao <liaohaitun@gmail.com>
2021-09-04 10:34:42 -0400

    jianzhi_斐波那次数列
    第一次没有取mod，看题都不看清。。。。

--- src/jianzhi_offer/easy.java
+++ src/jianzhi_offer/easy.java
@@ -10,26 +10,49 @@ class ListNode {
 public class easy {
     public static void main(String[] args) {
         easy t=new easy();
-        ListNode head=new ListNode(1);
-        ListNode n=head;
-        head.next=new ListNode(2);
-        head=head.next;
-        head.next=new ListNode(3);
-        head=head.next;
-        head.next=new ListNode(4);
-        head=head.next;
-        head.next=new ListNode(5);
-        head=head.next;
-        head.next=new ListNode(6);
-        int k=4;
-        ListNode tar=t.getKthFromEnd(n,k);
-        System.out.println(tar.val);
+        System.out.println(t.fib(45));
 
 
+
+    }
+    public int fib(int n) {
+        int f=0;
+        int s=1;
+        int target=1;
+        final int MOD = 1000000007;
+        if(n==0){
+            return 0;
+        }else if(n==1){
+            return 1;
+        }
+        f=1;
+        s=1;
+        for(int i=2;i<n;i++){
+            target=(f+s)%MOD;
+            f=s;
+            s=target;
+        }
+        return target;
     }
 
 
     public ListNode getKthFromEnd(ListNode head, int k) {
+        /**
+         * ListNode head=new ListNode(1);
+         *         ListNode n=head;
+         *         head.next=new ListNode(2);
+         *         head=head.next;
+         *         head.next=new ListNode(3);
+         *         head=head.next;
+         *         head.next=new ListNode(4);
+         *         head=head.next;
+         *         head.next=new ListNode(5);
+         *         head=head.next;
+         *         head.next=new ListNode(6);
+         *         int k=4;
+         *         ListNode tar=t.getKthFromEnd(n,k);
+         *         System.out.println(tar.val);
+         */
         int n=0;
         ListNode target=null;
         for(target=head;target!=null;target=target.next){
