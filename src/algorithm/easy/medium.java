3eb6237eed8707ed6d42ed201f405b8425fb1694
HaydenLiao <liaohaitun@gmail.com>
2021-10-12 18:56:51 -0400

    剑指 Offer 09. 用两个栈实现队列
    用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，
     * 分别完成在队列尾部插入整数和在队列头部删除整数的功能。
     * (若队列中没有元素，deleteHead操作返回 -1 )

--- src/algorithm/easy/medium.java
+++ src/algorithm/easy/medium.java
@@ -13,6 +13,43 @@ public class medium{
     }
 
 
+
+    //673. Number of Longest Increasing Subsequence
+    /**
+     * Given an integer array nums, return the number of longest increasing subsequences.
+     * Notice that the sequence has to be strictly increasing.
+     * eg:
+     * Input: nums = [1,3,5,4,7]
+     * Output: 2
+     * Explanation: The two longest increasing subsequences are [1, 3, 4, 7] and [1, 3, 5, 7].
+     *
+     * eg:
+     * Input: nums = [2,2,2,2,2]
+     * Output: 5
+     * Explanation: The length of longest continuous increasing subsequence is 1, and there are 5 subsequences' length is 1, so output 5.
+     *
+     */
+    public int findNumberOfLIS(int[] nums) {
+
+
+        return 0;
+
+    }
+
+    //300. Longest Increasing Subsequence
+    /**
+     * Given an integer array nums, return the length of the longest strictly increasing subsequence.
+     *
+     * A subsequence is a sequence that can be derived from an array by deleting some or no elements without changing the order of the remaining elements. For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
+     *
+     *
+     *
+     */
+
+
+
+
+
     //Q1894. Find the Student that Will Replace the Chalk
 
     /**
--- src/jianzhi_offer/easy.java
+++ src/jianzhi_offer/easy.java
@@ -1,11 +1,62 @@
 package jianzhi_offer;
 
 
+import java.util.Stack;
+
 class ListNode {
     int val;
     ListNode next;
     ListNode(int x) { val = x; }
 }
+
+
+//剑指 Offer 09. 用两个栈实现队列
+/**
+ * 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，
+ * 分别完成在队列尾部插入整数和在队列头部删除整数的功能。
+ * (若队列中没有元素，deleteHead操作返回 -1 )
+ */
+class CQueue {
+    /**
+     * Your CQueue object will be instantiated and called as such:
+     * CQueue obj = new CQueue();
+     * obj.appendTail(value);
+     * int param_2 = obj.deleteHead();
+     */
+
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    public CQueue() {
+        stack1=new Stack<>();
+        stack2=new Stack<>();
+    }
+
+    public void appendTail(int value) {
+        stack1.push(value);
+    }
+
+    public int deleteHead() {
+        if(!stack2.empty()){
+            return (stack2.pop());
+        }
+        else{
+            if(stack1.empty()){
+                return -1;
+            }
+            else{
+                while(!stack1.empty()){
+                    stack2.push(stack1.pop());
+                }
+                return stack2.pop();
+            }
+        }
+
+    }
+}
+
+
+
 
 public class easy {
     public static void main(String[] args) {
@@ -13,6 +64,11 @@ public class easy {
         System.out.println(t.fib(5));
 
     }
+
+
+
+
+
 
 
 
