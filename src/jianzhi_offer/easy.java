0c0a95104a99c07ab4b312d42eab2aec8e04018c
HaydenLiao <liaohaitun@gmail.com>
2021-10-13 11:53:32 -0400

    剑指 Offer 58 - II. 左旋转字符串

--- src/jianzhi_offer/easy.java
+++ src/jianzhi_offer/easy.java
@@ -187,6 +187,27 @@ public class easy {
     }
 
 
+    //剑指 Offer 58 - II. 左旋转字符串
+
+    /**
+     * 字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。
+     * 请定义一个函数实现字符串左旋转操作的功能。
+     * 比如，输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。
+     * @param s
+     * @param n
+     * @return
+     */
+    public String reverseLeftWords(String s, int n) {
+        String ret="";
+        for(int i=n;i<s.length();i++){
+            ret=ret+s.charAt(i);
+        }
+        for(int i=0;i<n;i++){
+            ret=ret+s.charAt(i);
+        }
+        return ret;
+
+    }
 
 
 
