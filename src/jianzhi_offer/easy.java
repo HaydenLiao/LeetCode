35c93874a6c0261bc5b071fe0f4cdb9c274d170e
HaydenLiao <liaohaitun@gmail.com>
2021-10-19 17:01:14 -0400

    剑指 Offer 11. 旋转数组的最小数字

--- src/jianzhi_offer/easy.java
+++ src/jianzhi_offer/easy.java
@@ -189,6 +189,35 @@ public class easy {
     }
 
 
+    //剑指 Offer 11. 旋转数组的最小数字
+    //https://leetcode-cn.com/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof/solution/mian-shi-ti-11-xuan-zhuan-shu-zu-de-zui-xiao-shu-3/
+
+    /**
+     * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+     * 输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。
+     * 例如，数组[3,4,5,1,2] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。
+     *
+     * 注意此数组为已排序数组，所以旋转之后定为递增序列
+     *
+     * 二分查找
+     */
+    public int minArray(int[] numbers) {
+        int i=0,j=numbers.length-1;
+        while(i<j){
+            int m=(i+j)/2;
+            if(numbers[m]>numbers[j]){
+                i=m+1;
+            }
+            else if(numbers[m]<numbers[j]){
+                j=m;
+            }
+            else
+                j--;
+        }
+        return numbers[i];
+    }
+
+
     //剑指 Offer 24. 反转链表
 
     /**
