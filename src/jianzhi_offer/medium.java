1ad71f28f7300de84fe7528c462930e900110c15
HaydenLiao <liaohaitun@gmail.com>
2021-10-21 12:22:21 -0400

    //剑指 Offer 04. 二维数组中的查找

--- src/jianzhi_offer/medium.java
+++ src/jianzhi_offer/medium.java
@@ -20,6 +20,37 @@ public class medium {
     }
 
 
+    //剑指 Offer 04. 二维数组中的查找
+    /**
+     * 在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，
+     * 每一列都按照从上到下递增的顺序排序。请完成一个高效的函数，
+     * 输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+     *
+     * 思路：1.暴力法遍历O(MN)
+     * 2.线性查找O(M+N)
+     */
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        int i=matrix.length-1;
+        int j=0;
+        boolean flag=false;
+        while(i>=0&&j<=matrix[0].length-1){
+            if(matrix[i][j]==target){
+                flag=true;
+                break;
+            }
+            if(matrix[i][j]>target){
+                i--;
+                continue;//要有continue，或者要用else if 去写
+            }
+            if(matrix[i][j]<target){
+                j++;
+                continue;
+            }
+        }
+        return flag;
+    }
+
+
     //剑指 Offer 35. 复杂链表的复制
 
     /**
@@ -29,8 +60,10 @@ public class medium {
      *
      * 解题思路：recursion+hashmap
      */
+    //注意此HashMap要在method外面建，否则会导致递归创建很多个hanshmap！！！！！！
+    Map<Node,Node> cacheNode=new HashMap<Node,Node>();//存储链表的原节点以及新拷贝的节点
     public Node copyRandomList(Node head) {
-        Map<Node,Node> cacheNode=new HashMap<Node,Node>();//存储链表的原节点以及新拷贝的节点
+
         if(head==null){
             return null;
         }
