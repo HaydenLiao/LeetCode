5e2c5784a3dd4e23606a3c6b589e4d39f5fb1e02
HaydenLiao <liaohaitun@gmail.com>
2021-10-26 13:35:03 -0400

    剑指 Offer 63. 股票的最大利润
    线性比较

--- src/jianzhi_offer/medium.java
+++ src/jianzhi_offer/medium.java
@@ -24,6 +24,10 @@ class TreeNode {
 
 public class medium {
     public static void main(String[] args){
+        medium m=new medium();
+        int[] array=new int[]{7,2,9,1,10};
+        int i=m.maxProfit(array);
+        System.out.println(i);
 
     }
 
@@ -206,6 +210,29 @@ public class medium {
             copyNode.random=copyRandomList(head.random);//递归肤质此节点的random节点
         }
         return cacheNode.get(head);
+    }
+
+    //剑指 Offer 63. 股票的最大利润
+
+    /**
+     *假设把某股票的价格按照时间先后顺序存储在数组中，
+     * 请问买卖该股票一次可能获得的最大利润是多少？
+     */
+    public int maxProfit(int[] prices) {
+        if (prices.length==0){
+            return 0;
+        }
+        int min=prices[0];
+        int curprof=0;
+        for(int i=1;i<prices.length;i++){
+          if( prices[i]-min>curprof ){
+             curprof=prices[i]-min;
+            }
+          if(prices[i]<min){
+              min=prices[i];
+          }
+        }
+        return curprof;
     }
 
 }
