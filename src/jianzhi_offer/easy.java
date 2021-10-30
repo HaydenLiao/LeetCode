512c108a1bc910bbb2ba315ff01b15d428e54b86
HaydenLiao <liaohaitun@gmail.com>
2021-10-29 22:59:16 -0400

    剑指 Offer 57. 和为s的两个数字
    双指针

--- src/jianzhi_offer/easy.java
+++ src/jianzhi_offer/easy.java
@@ -647,6 +647,32 @@ public class easy {
     }
 
 
+    //剑指 Offer 57. 和为s的两个数字
+    /**
+     * 输入一个递增排序的数组和一个数字s，在数组中查找两个数，使得它们的和正好是s。
+     * 如果有多对数字的和等于s，则输出任意一对即可。
+     */
+    public int[] twoSum(int[] nums, int target) {
+        int[] ret=new int[2];
+        int i=0;
+        int j=nums.length-1;
+        while(i<j){
+            if(nums[i]+nums[j]>target){
+                j--;
+            }
+            else if(nums[i]+nums[j]<target){
+                i++;
+            }
+            else {
+                ret[0]=nums[i];
+                ret[1]=nums[j];
+                break;
+            }
+        }
+        return ret;
+    }
+
+
 
 
     //剑指 Offer 58 - II. 左旋转字符串
